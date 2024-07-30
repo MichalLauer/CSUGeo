9281699af144456f843de047395b09fa16a4024d
Michal Lauer <michal.lauer.25@gmail.com>
2024-07-30 10:54:20 +0200

    Oprava prosince

--- R/get_date.R
+++ R/get_date.R
@@ -2,11 +2,17 @@ get_date <- function(year, month) {
   assert_int(year, lower = 2013)
   assert_int(month, lower = 1, upper = 12)
 
-  date <-
-    "%d-%02d-01" |> 
-    sprintf(year, month %% 12 + 1) |> 
-    as.Date() |> 
-    (\(x) x - 1)()
-
-  return(invisible(date))
+  if (month == 12) {
+    date <- as.Date(glue("{year}-{month}-31"))
+  } else {
+    date <-
+      "%d-%02d-01" |> 
+      sprintf(year, month %% 12 + 1) |> 
+      as.Date() |> 
+      (\(x) {
+        x - 1
+      })()
+  }
+  
+  return(as.Date(date))
 }
